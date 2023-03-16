ba9ad8ffa70341010a89f7bda18dc4d976c602d2
SproudSG <Grow@sproud.biz>
2023-03-16 10:54:16 +0800

    ik

---
+++ js/imports.js
@@ -0,0 +1,184 @@
+//start menu screen
+var myButton = document.getElementById("start-button");
+var fryNextButton = document.getElementById("fry-next-button");
+const fryingIntro = document.getElementById("video-container");
+const startUI = document.getElementById("startUI");
+const tutorialText = document.getElementById("tutorial-text");
+let nextBtnClickCount = 0;
+
+//Food picker main menu
+const foodPicker = document.getElementById("foodPicker");
+
+//Foods to pick from
+const tofu = document.getElementById("eggTofu")
+const duckBreast = document.getElementById("duckBreast")
+const batteredFish = document.getElementById("batteredFish")
+const spinach = document.getElementById("spinach")
+
+//Fry food screen
+const fryFood = document.getElementById("fryFood");
+
+//frying options
+const shallowFryingContainer = document.getElementById("shallowFryingContainer");
+const dryFryingContainer = document.getElementById("dryFryingContainer");
+const stirFryingContainer = document.getElementById("stirFryingContainer");
+const deepFryingContainer = document.getElementById("deepFryingContainer");
+
+
+//foods fried (completed)
+let completed = []
+
+// Start button
+myButton.onclick = function () {
+  startUI.style.display = "none";
+  fryingIntro.style.display = "block";
+  document.getElementById("fryingIntro").play();
+
+};
+
+
+// Next (frying intro) button
+fryNextButton.onclick = function () {
+  nextBtnClickCount++;
+  if (nextBtnClickCount === 1) {
+    tutorialText.textContent = "The main difference of the different frying methods is in the amount of fat or oil added. Learn more about frying in the following activity.​";
+  } else if (nextBtnClickCount === 2) {
+    fryingIntro.style.display = "none";
+    foodPicker.style.display = "block";
+  }
+};
+
+//Food choice and fry choice
+let foodChoice = "";
+let fryChoice = false;
+const criteriaText = document.getElementById("criteriaText");
+
+// FOOD BUTTON HANDLERS
+tofu.onclick = function () {
+  if (!completed.includes('tofu')) {
+    foodPicker.style.display = "none";
+    fryFood.style.display = "block";
+    //show the frying options
+    stirFryingContainer.style.display = 'block'
+    shallowFryingContainer.style.display = 'block'
+    dryFryingContainer.style.display = 'block'
+    deepFryingContainer.style.display = 'none'
+    criteriaText.innerHTML = "  After frying, the egg tofu should<br>    • maintain its shape; and <br>    • have a golden brown colour."
+
+    foodChoice = "tofu"
+  }
+
+};
+
+duckBreast.onclick = function () {
+  foodPicker.style.display = "none";
+  fryFood.style.display = "block";
+  //show the frying options
+  deepFryingContainer.style.display = "block";
+  dryFryingContainer.style.display = "block";
+  stirFryingContainer.style.display = 'none'
+  shallowFryingContainer.style.display = 'none'
+  criteriaText.innerHTML = " After frying, the smoked duck breast should: <br> • not see an increase in its fat content; and  <br> • have a desirable flavour and crispy texture."
+
+  foodChoice = "duck"
+};
+
+batteredFish.onclick = function () {
+
+};
+
+spinach.onclick = function () {
+
+};
+
+
+
+// frying button handlers
+const dryFrying = document.getElementById("dryFrying")
+const deepFrying = document.getElementById("deepFrying")
+const shallowFrying = document.getElementById("shallowFrying")
+const stirFrying = document.getElementById("stirFrying")
+const fryingFoodVidContainer = document.getElementById("food-video-container")
+
+//Tofu Vids
+const shallowFryTofuVid = document.getElementById("shallowFryTofuVid")
+const dryFryTofuVid = document.getElementById("dryFryTofuVid")
+
+const fryFinishBtn = document.getElementById("fry-finish-button")
+
+
+
+deepFrying.onclick = function () {
+  if (foodChoice === "duck") {
+    console.log("wrong choice")
+  }
+};
+dryFrying.onclick = function () {
+  if (foodChoice === "tofu") {
+    console.log("wrong choice")
+    fryFood.style.display = "none";
+    fryingFoodVidContainer.style.display = "block";
+    dryFryTofuVid.style.display = "block";
+    dryFryTofuVid.play();
+    fryChoice = "dry"
+
+  } else if (foodChoice === "duck") {
+    console.log("right choice")
+  }
+};
+shallowFrying.onclick = function () {
+  if (foodChoice === "tofu") {
+    console.log("right choice")
+    fryChoice = true
+    fryFood.style.display = "none";
+    fryingFoodVidContainer.style.display = "block";
+    shallowFryTofuVid.style.display = "block";
+
+    shallowFryTofuVid.play();
+  }
+};
+stirFrying.onclick = function () {
+  if (foodChoice === "tofu") {
+    console.log("wrong choice")
+  }
+};
+
+//TOFU VID ENDED EVENT LISTENERS
+shallowFryTofuVid.addEventListener('ended', function () {
+  fryFinishBtn.style.display = "block";
+  completed.push("tofu")
+  fryFinishBtn.textContent = "BACK TO MENU"
+
+});
+
+dryFryTofuVid.addEventListener('ended', function () {
+  fryFinishBtn.style.display = "block";
+  fryFinishBtn.textContent = "TRY AGAIN"
+
+});
+
+fryFinishBtn.onclick = function () {
+  if (fryChoice) {
+    fryingFoodVidContainer.style.display = "none";
+    foodPicker.style.display = "block";
+    if (completed.includes('tofu')) {
+      shallowFryTofuVid.style.display = "none";
+      document.getElementById("eggTofuContainer").classList.remove('food-item');
+      document.getElementById("eggTofuContainer").classList.add('opacity');
+    }
+    fryFinishBtn.style.display = "none";
+  } else {
+    if (foodChoice === "tofu") {
+
+      fryFinishBtn.style.display = "none";
+      fryingFoodVidContainer.style.display = "none";
+      fryFood.style.display = "block";
+
+      if(fryChoice === "dry"){
+        dryFryTofuVid.style.display = "none";
+        dryFrying.classList.add('opacity');
+      }
+
+    }
+  }
+};
